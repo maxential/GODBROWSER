45840cf9f85cafc902c579652d3aca67940189bb
maxential <154645486+maxential@users.noreply.github.com>
2024-02-23 11:35:49 -0800

    first commitee

---
+++ GODBROWSER/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DanTup.BrowserSelector
+{
+	class Program
+	{
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 1 && (args[0] == "--register" || args[0] == "--unregister"))
+            {
+                EnsureAdmin("--register");
+                return;
+            }
+
+            if (args.Length == 1 && args[0] == "--create")
+            {
+                CreateSampleSettings();
+                return;
+            }
+
+            // Show GUI with registration button
+            ShowRegistrationBox();
+        }
+
+        static void ShowRegistrationBox()
+        {
+            DialogResult result = MessageBox.Show("Click 'Register Browser' to set the browser as default.",
+                                                  "Browser Registration", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (result == DialogResult.OK)
+            {
+                string selectedPath = ChooseExePath();
+
+                if (!string.IsNullOrEmpty(selectedPath))
+                {
+                    EnsureAdmin("--register");
+                    RegistrySettings.RegisterBrowser(selectedPath);
+
+                    // Show "Done!" message box
+                    MessageBox.Show("Done!", "Registration Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+
+        static string ChooseExePath()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Executable Files|*.exe";
+                openFileDialog.Title = "Select the Browser Executable";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+
+        static void EnsureAdmin(string arg)
+		{
+			WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+			if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+			{
+				Process.Start(new ProcessStartInfo
+				{
+					FileName = Assembly.GetExecutingAssembly().Location,
+					Verb = "runas",
+					Arguments = arg
+				});
+				Environment.Exit(0);
+			}
+		}
+
+
+
+		static void CreateSampleSettings()
+		{
+			DialogResult r = DialogResult.Yes;
+
+			if (File.Exists(ConfigReader.ConfigPath))
+			{
+				r = MessageBox.Show(@"The settings file already exists. Would you like to replace it with the sample file? (The existing file will be saved/renamed.)", "BrowserSelector", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+			}
+
+			if (r == DialogResult.No)
+				return;
+
+			ConfigReader.CreateSampleIni();
+		}
+	}
+}
---
+++ GODBROWSER/RegistrySettings.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace DanTup.BrowserSelector
+{
+    static class RegistrySettings
+    {
+        const string AppID = "DanTup.BrowserSelector";
+        const string AppName = "DanTup's Browser Selector";
+        const string AppDescription = "DanTup's Browser Selector";
+
+        internal static void RegisterBrowser(string customExePath)
+        {
+            string appPath = Assembly.GetExecutingAssembly().Location;
+            string appIcon = appPath + ",0";
+            string appOpenUrlCommand = customExePath + " %1";
+
+            // Extract the executable name from the path
+            string exeName = Path.GetFileNameWithoutExtension(customExePath);
+
+            // Register application.
+            var appReg = Registry.LocalMachine.CreateSubKey($"SOFTWARE\\{exeName}");
+
+            // Register capabilities.
+            var capabilityReg = appReg.CreateSubKey("Capabilities");
+            capabilityReg.SetValue("ApplicationName", exeName);
+            capabilityReg.SetValue("ApplicationIcon", appIcon);
+            capabilityReg.SetValue("ApplicationDescription", $"{exeName} Web Browser");
+
+            // Set up protocols we want to handle.
+            var urlAssocReg = capabilityReg.CreateSubKey("URLAssociations");
+            urlAssocReg.SetValue("http", $"{exeName}URL");
+            urlAssocReg.SetValue("https", $"{exeName}URL");
+            urlAssocReg.SetValue("ftp", $"{exeName}URL");
+
+            // Register as application.
+            Registry.LocalMachine.OpenSubKey("SOFTWARE\\RegisteredApplications", true).SetValue(exeName, $"SOFTWARE\\{exeName}\\Capabilities");
+
+            // Set URL Handler.
+            var handlerReg = Registry.LocalMachine.CreateSubKey($"SOFTWARE\\Classes\\{exeName}URL");
+            handlerReg.SetValue("", exeName);
+            handlerReg.SetValue("FriendlyTypeName", $"{exeName} Web Browser");
+
+            handlerReg.CreateSubKey("shell\\open\\command").SetValue("", appOpenUrlCommand);
+        }
+
+        internal static void UnregisterBrowser()
+        {
+            Registry.LocalMachine.DeleteSubKeyTree(string.Format("SOFTWARE\\{0}", AppID), false);
+            Registry.LocalMachine.OpenSubKey("SOFTWARE\\RegisteredApplications", true).DeleteValue(AppID);
+            Registry.LocalMachine.DeleteSubKey(string.Format("SOFTWARE\\Classes\\{0}URL", AppID));
+        }
+    }
+}
